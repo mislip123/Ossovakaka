7af5888fe851b1ed7a5b7c67d463f86298d1e3a9
Mischa <Mischalippmann@gmail.com>
2023-04-24 23:24:20 +0200

    Docker

---
+++ applicatie/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="nl">
+    <head>
+        <title>KiKaKankerVakantie</title>
+        <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="Logo.jpg">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="StyleSheet.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
