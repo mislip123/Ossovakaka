029f417bb1773b8de14163f3918b97ca7fb3d652
Mischa <Mischalippmann@gmail.com>
2023-04-25 10:01:50 +0200

    Navigatie balk en vreetschuur pagina

--- applicatie/index.php
+++ applicatie/index.php
@@ -8,6 +8,13 @@
         <link rel="stylesheet" href="CSS\StyleSheet.css">
     </head>
     <body class="ossovakaka">
+        <nav class="topbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="vreetschuur.php">Vreetschuur</a></li>
+            </ul>
+        </nav>
+
         <div class="izjen_titel">
             <h class="titel">COUNTDOWN TOT OSSOVACANTION 2.0</h>
         </div>
---
+++ applicatie/vreetschuur.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="nl">
+    <head>
+        <title>KiKaKankerVakantie</title>
+        <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="..\Images\logo.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="CSS\StyleSheet.css">
+    </head>
+    <body class="ossovakaka">
+        <nav class="topbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="vreetschuur.php">Vreetschuur</a></li>
+            </ul>
+        </nav>
+
+        <div class="izjen_titel">
+            <h class="titel">VREETSCHUURTJES</h>
+        </div>
+    </body>
+</html>
